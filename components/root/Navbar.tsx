5da7d061c9c73cc5d9ad764c994fc5ca2d8a2b6a
ronish <ronish.rohan@gmail.com>
2025-10-26 00:15:15 +0530

    feat: add automatic resizing on type

--- components/root/Navbar.tsx
+++ components/root/Navbar.tsx
@@ -11,7 +11,11 @@ export default function Navbar() {
       <div className="text-2xl tracking-[-4px] font-light ">
         VI<span className="italic">CHAAR</span>
       </div>
-      <div className="ml-auto h-full w-[300px] relative group">
+      <motion.div
+        transition={{ ease: "easeInOut", duration: 0.3 }}
+        animate={{ width: query.length > 4 ? "400px" : "300px" }}
+        className="ml-auto h-full  relative group"
+      >
         <div className="absolute left-4 top-1/2  -translate-y-1/2 text-stone-500 text-sm flex gap-2 items-center pointer-events-none">
           <MagnifyingGlassIcon className="group-focus-within:text-stone-900 transition-colors duration-300" />
           <span
@@ -32,9 +36,9 @@ export default function Navbar() {
           initial={{ height: "200%" }}
           animate={{ height: focused || query.length > 0 ? "100%" : "200%" }}
           transition={{ ease: "circInOut", duration: 0.3 }}
-          className="w-full  rounded-2xl absolute bottom-0 duration-300 left-0 group-focus-within:border-stone-700 transition-shadow group-focus-within:shadow-xl shadow-black/10 border border-border pointer-events-none"
+          className="w-full  rounded-2xl absolute bottom-0 duration-300 left-0 group-focus-within:border-stone-700 transition-shadow group-focus-within:shadow-xl shadow-stone-300/50 border border-border pointer-events-none"
         ></motion.div>
-      </div>
+      </motion.div>
     </div>
   );
 }

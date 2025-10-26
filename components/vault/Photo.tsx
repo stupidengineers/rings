52346725e8635c8a94fcbe7d9bf4eab5b0796d90
ronish <ronish.rohan@gmail.com>
2025-10-26 18:55:55 +0530

    feat: add content in photo card

--- components/vault/Photo.tsx
+++ components/vault/Photo.tsx
@@ -1,11 +1,27 @@
 "use client";
 import React from "react";
 import Card from "./Card";
+import { motion } from "framer-motion";
 
 export default function Photo() {
   return (
     <Card>
       <div className="w-full h-fit flex select-none relative rounded-3xl overflow-hidden">
+        <div className="p-2 absolute bottom-0 w-full">
+          <motion.div
+            variants={{
+              rest: { y: "120%", scaleY: 0.2 },
+              hover: { y: "0%", scaleY: 1 },
+            }}
+            transition={{
+              duration: 0.3,
+              ease: "circInOut",
+            }}
+            className="text-base px-4 py-3 rounded-2xl bottom-0 w-full bg-white backdrop-blur-[2px]"
+          >
+            this photo is lowkey hard as fuck wish i could save it somewhere
+          </motion.div>
+        </div>
         <div className="w-full h-fit">
           <img
             draggable={false}
